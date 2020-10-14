735719110413197e556cc0f9b02c50aa8b538872
pedroso_pedro@hotmail.com <32OLfCDTPTkd>
2020-10-14 22:52:33 +0100

    Finished Basic: Call

--- call.js
+++ call.js
@@ -1,5 +1,6 @@
-function duckCount() {
-    // SOLUTION GOES HERE
-  }
 
+function duckCount() 
+{
+  return Array.prototype.slice.call(arguments).filter(x => Object.prototype.hasOwnProperty.call(x,'quack')).length;
+}
 module.exports = duckCount
--- start.js
+++ start.js
@@ -1,13 +1,8 @@
-const inspect = require("./tpc/tpc2-reflection");
+'use strict'
+
+const duckCount = require("./call");
 
 
-function Fruit(name,color) {
-  this.name = name
-  this.color = color
-  this.toString = function(){ return `${this.name} with the color ${this.color}`}
-  this.changeColor = function(color) {this.color = color}
-}
-
-let apple = new Fruit('Apple','red')
-
-inspect(apple);
+var notDuck = Object.create({quack: true})
+var duck = {quack: true}
+console.log(duckCount(duck, notDuck)); // 1
