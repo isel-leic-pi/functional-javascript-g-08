8ea53e9c7aee84a37f8f98685616b53333d22db6
PPedroso <pedroso_pedro@hotmail.com>
2020-10-15 19:27:45 +0100

    Testing user

--- 32632/functional-javascripting/start.js
+++ 32632/functional-javascripting/start.js
@@ -7,6 +7,7 @@ var info = logger('INFO:')
     info('this is an info message')
     // INFO: this is an info message
 
+    
     var warn = logger('WARN:')
     warn('this is a warning message', 'with more info')
     // WARN: this is a warning message with more info
