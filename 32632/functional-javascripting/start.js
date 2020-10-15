59ef0648e1ce7c3b83428d8a642828bf84f23285
pedroso_pedro@hotmail.com <32OLfCDTPTkd>
2020-10-15 06:50:05 +0100

    Added scripts from first homework. Improved the folder structure

---
+++ 32632/functional-javascripting/start.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const logger = require("./partial-application");
+
+
+var info = logger('INFO:')
+    info('this is an info message')
+    // INFO: this is an info message
+
+    var warn = logger('WARN:')
+    warn('this is a warning message', 'with more info')
+    // WARN: this is a warning message with more info
--- start.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const duckCount = require("./call");
-
-
-var notDuck = Object.create({quack: true})
-var duck = {quack: true}
-console.log(duckCount(duck, notDuck)); // 1
