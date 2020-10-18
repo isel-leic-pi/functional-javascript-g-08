8940b729d41626888c61d70563b448e1a19ba28a
PPedroso <pedroso_pedro@hotmail.com>
2020-10-18 10:55:11 +0100

    finished ex 12

--- 32632/functional-javascripting/function-spies.js
+++ 32632/functional-javascripting/function-spies.js
@@ -1,10 +1,44 @@
+var slice = Array.prototype.slice
+
 function Spy(target, method) {
     
-    target[method] = function() {
-        let x = 0;
-
-        
+    /**
+     * This is a global variable. There should be a better way to do this
+     */
+    count = 0;
+    
+    let func = target[method]
+    
+    target[method] = function()
+    {
+        count++
+        func.call(this,arguments[0]) /** call VS apply ?? */ /** Does it work with function with more than one argument? */
     }
+
+    return this
   }
 
   module.exports = Spy
+
+
+  /**   PROVIDED SOLUTION
+   * 
+   * function Spy(target, method) {
+      var originalFunction = target[method]
+
+      // use an object so we can pass by reference, not value
+      // i.e. we can return result, but update count from this scope
+      var result = {
+        count: 0
+      }
+
+      // replace method with spy method
+      target[method] = function() {
+        result.count++ // track function was called
+        return originalFunction.apply(this, arguments) // invoke original function
+      }
+
+      return result
+    }
+   * 
+   */
--- 32632/functional-javascripting/partial-application-with-bind.js
+++ 32632/functional-javascripting/partial-application-with-bind.js
@@ -1,4 +1,4 @@
-var slice = Array.prototype.slice
+
 
 module.exports = function(namespace) {
     return console.log.bind(null, namespace)
