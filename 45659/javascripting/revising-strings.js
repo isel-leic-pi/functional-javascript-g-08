db9797b26b7bb29d6804057a28637cfd13dabb71
Vasco Bonifácio Costa <vascobonifacio@gmail.com>
2020-10-22 21:35:06 +0100

    45659: javascripting -1-4

---
+++ 45659/javascripting/revising-strings.js
@@ -0,0 +1,7 @@
+'use strict'
+
+let pizza = 'pizza is alright'
+
+pizza = pizza.replace('alright','wonderful')
+
+console.log(pizza)
---
+++ 45659/javascripting/variables.js
@@ -0,0 +1,5 @@
+'use strict'
+
+let variable = 'some string'
+
+console.log(variable)
